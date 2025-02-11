557d27d9d14d165295ac65811157a28803d1aa8c
Anineco Nyanta <anineco@nifty.com>
2025-02-11 09:37:46 +0900

    extract xml

--- mksql.sh
+++ mksql.sh
@@ -1,9 +1,13 @@
-#!/bin/bash -eu
+#!/opt/local/bin/bash -eu
 
 cd results
 rm -f x_*.sql
 
-sort ../output/*.txt | uniq | ../bsplit.pl 16
+(
+../xml2txt.py ../latest/*-GCP-*.xml
+../xml2txt.py ../latest/*-ElevPt-*.xml
+) | ../bsplit.pl 16
+
 for x in x_*.sql; do
   mv $x $x.bak
   echo 'INSERT INTO `gcp` VALUES' > $x
--- proc.sh
+++ proc.sh
@@ -1,42 +1,47 @@
-#!/bin/bash -eu
-# macports: p7zip
+#!/opt/local/bin/bash -eu
+# requires 7z in package 'p7zip' in macports
 
-ODIR=output
+rm -rf temp latest
+mkdir -p temp latest
 
-rm -rf temp $ODIR
-mkdir -p temp $ODIR
+# Extract GCP xml files from 01 cab files
 
 for c in resources/*-01-*.cab; do
   CAB=$(basename $c .cab)
   TMPCAB=temp/$CAB
   7z x -o$TMPCAB $c '*-01-*'
-  for z in $TMPCAB/*.zip; do
-    set +e
-    unzip $z '*-GCP-*' -d $TMPCAB
-    set -e
-  done
-  rm -f $ODIR/$CAB.txt
-  for x in $TMPCAB/*.xml; do
-    echo $x
-    ./xml2txt.py $x >> $ODIR/$CAB.txt
-  done
-  rm -rf $TMPCAB
 done
 
-for c in resources/*-02-*; do
+declare -A zip01
+
+for z in temp/*/FG-GML-*-01-*.zip; do
+  b=${z##*/}
+  if [ -z "${zip01[$b]:-}" ]; then
+    set +e
+    unzip $z '*-GCP-*' -d latest
+    set -e
+    zip01[$b]=$z
+  fi
+done
+
+# Extract ElevPt xml files from 02 cab files
+
+for c in resources/*-02-*.cab; do
   CAB=$(basename $c .cab)
   TMPCAB=temp/$CAB
-  OUTPUT=output/$CAB.txt
   7z x -o$TMPCAB $c '*-09-*'
-  for z in $TMPCAB/*.zip; do
-    set +e
-    unzip $z '*-ElevPt-*' -d $TMPCAB
-    set -e
-  done
-  rm -f $ODIR/$CAB.txt
-  for x in $TMPCAB/*.xml; do
-    echo $x
-    ./xml2txt.py $x >> $ODIR/$CAB.txt
-  done
-  rm -rf $TMPCAB
 done
+
+declare -A zip09
+
+for z in temp/*/FG-GML-*-09-*.zip; do
+  b=${z##*/}
+  if [ -z "${zip09[$b]:-}" ]; then
+    set +e
+    unzip $z '*-ElevPt-*' -d latest
+    set -e
+    zip09[$b]=$z
+  fi
+done
+
+rm -rf temp
